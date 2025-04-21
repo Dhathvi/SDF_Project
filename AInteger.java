b6d4458760b0342410c1472ebeacbf28bb8bf790
Dhathvi Charan <cs24btech11016@iith.ac.in>
2025-04-21 14:48:43 +0530

    Defined add functionality.

--- AInteger.java
+++ AInteger.java
@@ -29,6 +29,36 @@ public class AInteger {
     }
 
     public AInteger add(AInteger val){
+        String num1 = this.value;
+        String num2 = val.value;
+
+        int carry=0;
+        StringBuilder result= new StringBuilder();
+        int maxlength=0;
+
+        if (num1.length()>num2.length()){
+            maxlength=num1.length();
+        } else {
+            maxlength=num2.length();
+        }
+
+        num1 = new StringBuilder(num1).reverse().toString(); // Reverse the first number
+        num2 = new StringBuilder(num2).reverse().toString(); // Reverse the second number
+
+        for(int i=0;i<maxlength;i++){
+            int digit1 = i<num1.length() ? num1.charAt(i)-'0' : 0 ;
+            int digit2 = i<num2.length() ? num2.charAt(i)-'0' : 0 ;
+
+            int sum = digit1 + digit2 + carry;
+
+            carry=sum/10;
+            result.append(sum%10);
+        }
         
+        if (carry != 0) {
+            result.append(carry); // If there's a remaining carry, append it
+        }
+
+        return new AInteger(result.reverse().toString());
     }
 }
